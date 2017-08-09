01f66b6db1ffb1855cd3cee05dd4818d0ac9c202
kkkkong <kong.k@arms-thai.com>
2017-08-09 23:11:27 +0700

    update process for validate

--- Website.php
+++ Website.php
@@ -6,11 +6,13 @@
         public function __construct ()
         {
             parent::__construct();
+            $this->load->library('form_validation');
         }
 
         public function index()
         {
-            $this->load->view('form');
+                $this->load->view('form');
+
         }
 
         public function submission()
@@ -18,23 +20,43 @@
 
             if (!$this->input->is_ajax_request()) { exit('no valid req.'); }
 
-            $FormRules = array(
-                array(
-                    'field' => 'website',
-                    'label' => 'Website Address',
-                    'rules' => 'required|min_length[5]|max_length[30]'
-                )
-            );
 
-            $this->form_validation->set_rules($FormRules);
+            $this->form_validation->set_rules('username', 'Username', 'callback_username_check');
+            $this->form_validation->set_rules('password', 'Password', 'required');
+            $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+            $this->form_validation->set_rules('email', 'Email', 'required');
 
             if ($this->form_validation->run() == TRUE)
             {
-                echo '<div class="success">Your website is submitted</div>';
+                $data_msg['err'] = '<div class="success">Your website is submitted</div>';
+                $data_msg['flag'] = true;
+
+                $data_msg = json_encode($data_msg);
+
+                echo $data_msg;
             }
             else
             {
-                echo '<div class="errors">'.validation_errors().'</div>';
+
+                $data_msg['err'] = '<div class="errors">'.validation_errors().'</div>';
+                $data_msg['flag'] = false;
+
+                $data_msg = json_encode($data_msg);
+
+                echo $data_msg;
+            }
+        }
+
+        public function username_check($str)
+        {
+            if ($str == 'test')
+            {
+                $this->form_validation->set_message('username_check', 'The %s field can not be the word "test"');
+                return FALSE;
+            }
+            else
+            {
+                return TRUE;
             }
         }
 
